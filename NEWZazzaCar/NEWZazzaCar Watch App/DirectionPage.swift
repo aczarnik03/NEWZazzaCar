897549e7af9ef1df244d07e410f0c5099cfca1fa
ppauly03 <phionaceleste@gmail.com>
2025-04-09 14:02:47 +0200

    Update DirectionPage.swift
    South

--- NEWZazzaCar/NEWZazzaCar Watch App/DirectionPage.swift
+++ NEWZazzaCar/NEWZazzaCar Watch App/DirectionPage.swift
@@ -7,12 +7,55 @@
 
 import SwiftUI
 
-struct DirectionPage: View {
+struct TriangleWithCircle: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            VStack(spacing: -200) {
+                // Rounded triangle with gradient, rotated to the right
+                RoundedTriangle()
+                    .fill(
+                        LinearGradient(
+                            gradient: Gradient(colors: [Color.green, Color.black]),
+                            startPoint: .top,
+                            endPoint: .bottom
+                        )
+                    )
+                    .frame(width: 200, height: 200)
+                    .position(x: 100, y: 50)
+                    .rotationEffect(.degrees(180)) // Rotated right
+                // Green circle
+                Circle()
+                    .fill(Color.green)
+                    .frame(width: 100, height: 100)
+                    .position(x: 100, y: 100)
+            }
+        }
     }
 }
 
-#Preview {
-    DirectionPage()
+// Custom triangle with rounded top
+struct RoundedTriangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        let radius: CGFloat = 20
+        path.move(to: CGPoint(x: rect.minX + radius, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.maxX - radius, y: rect.minY))
+        path.addQuadCurve(to: CGPoint(x: rect.maxX, y: rect.minY + radius),
+                          control: CGPoint(x: rect.maxX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.midX + 1, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX - 1, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.minY + radius))
+        path.addQuadCurve(to: CGPoint(x: rect.minX + radius, y: rect.minY),
+                          control: CGPoint(x: rect.minX, y: rect.minY))
+        return path
+    }
 }
+
+struct TriangleWithCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        TriangleWithCircle()
+    }
+}
+
